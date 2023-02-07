9553b9911e348d29f1fe5f77cbe0c1aa8d3c3855
Pedro <pedri397@gmail.com>
2023-02-07 17:06:50 +0100

    PortFolio animated

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/** Hacer que se muevan con scroll */
+
+
+// let box__mover = document.querySelectorAll('.box__mover')
+
+
+
+// const movercajas =()=> {
+//    let posicion = window.innerHeight / 5 * 4
+
+//    box__mover.forEach(box => {
+//     let boxtop = box.getBoundingClientRect().top
+
+//     console.log(boxtop)
+    
+//     if(boxtop < posicion){
+//         box.classList.add("box__enseñar")
+//     }else {
+//         box.classList.remove("box__enseñar")
+//     }
+//    })
+// }
+
+
+
+// window.addEventListener("scroll", movercajas)
+
+
+/** --------------------------------------- */
